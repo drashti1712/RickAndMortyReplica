cc16be4ee3dbca97a6eac4048f9546ae7e7758e3
Hritikdusad <16ucs078@lnmiit.ac.in>
2020-08-16 22:44:56 +0530

    Initial folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import './App.css';
+import MainComponent from './Components/MainComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainComponent />
   );
 }
 
