ddc2d63c796098ba65633c70ed16c3a380bb2f02
drashti1712 <drashti1712@gmail.com>
2020-08-17 20:18:32 +0530

    added title and background

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import TitleComponent from './TitleComponent'
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from '../Theme';
 
 export default function MainComponent() {
     return (
-        <div>
-            <h1>Sample Component</h1>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div>
+                <TitleComponent />
+                <h1>Sample Component</h1>
+            </div>
+        </ThemeProvider>
     )
 }
