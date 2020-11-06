5507c13cda0c96fb0004197d7669f5c434aec4f5
drashti1712 <drashti1712@gmail.com>
2020-11-07 03:58:14 +0530

    Resolved issues of prev commit

--- README.md
+++
@@ -1,1 +0,0 @@
-Logo link- https://rickandmortyapi.com/favicon-32x32.png?v=1538abef51e33ef514e8fe1ab9aeab4e
--- src/Components/CardComponents/EpisodeCard.js
+++ src/Components/CardComponents/EpisodeCard.js
@@ -1,30 +1,39 @@
 import React from 'react'
-import {Grid, Typography, makeStyles, Paper} from "@material-ui/core";
+import {Grid, makeStyles, Paper} from "@material-ui/core";
+// import { render } from '@testing-library/react';
 
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        height: 250,
-        width: 200,
-        backgroundColor:"#3c3e44"
-    },
-}));
+// const useStyles = makeStyles((theme) => ({
+//     paper: {
+//         height: 250,
+//         width: 200,
+//         backgroundColor:"#3c3e44"
+//     },
+// }));
 
-const EpisodeCard = () => {
-    const classes = useStyles();
-    return(
-        <Grid item xs={12}>
-            <Grid container justify="center" spacing={4}>
-                {[0, 1, 2, 3, 4].map((value) => (
-                    <Grid key={value} item>
-                        <Paper className={classes.paper}>
-                            
-                        </Paper>
-                    </Grid>
-                ))}
+class EpisodeCard extends React.Component {
+    // const classes = useStyles();
+    
+    render(){
+        // const classes = useStyles();
+        const style = {
+            height: 250,
+            width: 200,
+            backgroundColor:"#3c3e44"
+        };
+        return(
+            <Grid item xs={12}>
+                <Grid container justify="center" spacing={4}>
+                    {[0, 1, 2, 3, 4].map((value) => (
+                        <Grid key={value} item>
+                            <Paper stlye={style}>
+                                
+                            </Paper>
+                        </Grid> 
+                    ))}
+                </Grid>
             </Grid>
-        </Grid>
-    );
-
+        );
+    }
 }
 
 export default EpisodeCard;
